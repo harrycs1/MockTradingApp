2cde15e5ef741453bae6646611443e6db94029ea
harrycs1 <98349746+harrycs1@users.noreply.github.com>
2023-03-13 23:50:18 +0000

    commit 5 - duplicate user bug fixed

--- app.py
+++ app.py
@@ -27,14 +27,11 @@ Session(app)
 # create a Connection object that represents the SQLite database
 con = sqlite3.connect("finance.db", check_same_thread=False)
 
-# Configure CS50 Library to use SQLite database
-# db = SQL("sqlite:///finance.db")
-
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
 
-
+# Ensure responses aren't cached
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -48,83 +45,105 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
+
+    # Query database to select portfolio data from transaction table, and select user cash balance from users table
     with con:
         cur = con.cursor()
         cur.execute("SELECT symbol, SUM(shares) AS shares, SUM(transaction_total), (current_price * SUM(shares)), name, current_price, ((current_price*shares) - transaction_total) FROM transactions WHERE buyer = ? GROUP BY symbol HAVING sum(shares) > 0", [session["user_id"]])
-        rows = cur.fetchall()
+        portfolio_data = cur.fetchall()
         cur.execute("SELECT cash FROM users WHERE id = ?", [session["user_id"]])
-        cash_db = cur.fetchone()
+        cash_balance = cur.fetchone()[0]
 
-    cash_value = cash_db[0] #cash row in users table
-    total = 0
+    # Assign total portfolio value variable
+    total_portfolio_value = 0
 
-    for row in rows:
-        result = lookup(row[0])
-        current_stock_price = result["price"]
+    # For each row (stock) in users portfolio
+    for row in portfolio_data:
+
+        # Get total current value of total shares owned and calculate total portolio value (without cash)
+        quote = lookup(row[0])
+        current_stock_price = quote["price"]
         total_stock_value = row[1] * current_stock_price
-        total += total_stock_value
+        total_portfolio_value += total_stock_value
 
-    total += cash_value
-    
-    for row in rows:
-        result = lookup(row[0])
-        current_stock_price = result["price"]
-        stock_symbol = result["symbol"]
+        # Query database to update current share price of stocks in portfolio in transactions table
+        stock_symbol = quote["symbol"]
         with con:
             cur = con.cursor()
             cur.execute("UPDATE transactions SET current_price = ? WHERE symbol = ?", (current_stock_price, stock_symbol))
 
+    # Get total portfolio value including cash balance
+    total_portfolio_value += cash_balance
+
+    # Query database to select unrealised profit/loss from transactions table
     with con:
         cur = con.cursor()
         cur.execute("SELECT IFNULL(SUM(((current_price*shares)-transaction_total)), 0) FROM transactions WHERE buyer = ? ", [session["user_id"]])
         sum_difference = cur.fetchone()[0]
 
-    return render_template("index.html", database = rows, cash = cash_value, total = total, PL = sum_difference)
+    return render_template("index.html", database = portfolio_data, cash = cash_balance, total = total_portfolio_value, PL = sum_difference)
 
 
 @app.route("/buy", methods=["GET", "POST"])
+# Login required decorator to ensure page can only be viewed by users that are logged in
 @login_required
 def buy():
     """Buy shares of stock"""
+    # User reached route via GET (as by clicking a link or via redirect)
     if request.method == "GET":
         return render_template("buy.html")
+    
+    # User reached route via POST (as by submitting a form via POST)
     else:
         symbol = request.form.get("symbol")
-        shares = int(request.form.get("shares"))
+        shares = float(request.form.get("shares"))
 
+        # Ensure symbol was submitted
         if not symbol:
             return apology("Must enter shares")
 
+        # Ensure that a whole number of shares was submitted, shares must be a string to use the isdigit() method
         if not request.form.get("shares").isdigit():
             return apology("Can't buy fractional shares")
 
+        # Ensure valid number of shares was submitted
         if not shares or shares < 0:
             return apology("Shares must be greater than zero")
 
-        stock = lookup(symbol.upper())
+        # Get quote for the stock being purchased
+        quote = lookup(symbol.upper())
 
-        if not stock:
+        # Ensure stock exists
+        if not quote:
             return apology("Symbol not found")
 
-        amount_due = stock["price"] * float(shares)
+        # Get amount due for transaction
+        amount_due = quote["price"] * float(shares)
 
+        # Query database to select user cash balance from users table
         with con:
             cur = con.cursor()
             cur.execute("SELECT cash FROM users WHERE id = ?", [session["user_id"]])
             balance = cur.fetchone()[0]
 
+        # Get time of transaction
         time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+        # Ensure sufficient funds are available for transaction
         if amount_due > balance:
             return apology("Insufficient funds")
         else:
+
+            # Get updated user cash balance
             uptd_cash = balance - amount_due
 
+            # Query database to insert transaction data in transactions table, and update user cash balace in users table
             with con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO transactions (buyer, name, symbol, transaction_price, shares, date, transaction_total, current_price) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (session["user_id"], stock["name"], stock["symbol"], stock["price"], shares, time, amount_due, stock["price"]))
+                cur.execute("INSERT INTO transactions (buyer, name, symbol, transaction_price, shares, date, transaction_total, current_price) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (session["user_id"], quote["name"], quote["symbol"], quote["price"], shares, time, amount_due, quote["price"]))
                 cur.execute("UPDATE users SET cash = ? WHERE id = ?", (uptd_cash, session["user_id"]))
 
+            # Show flash message upon complete transaction
             flash(f"Bought for {usd(amount_due)}!")
             return redirect("/")
 
@@ -134,7 +153,10 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
+    # User reached route via GET (as by clicking a link or via redirect)
     if request.method == "GET":
+        
+        # Query database to select all user transactions from users table
         with con:
             cur = con.cursor()
             cur.execute("SELECT * FROM transactions WHERE buyer = ?", [session["user_id"]])
@@ -147,28 +169,39 @@ def history():
 @login_required
 def addCash():
     """Add more cash"""
+
+    # User reached route via GET (as by clicking a link or via redirect)
     if request.method == "GET":
         return render_template("addCash.html")
+    
+    # User reached route via POST (as by submitting a form via POST)
     else:
-        newCash = int(request.form.get("newCash"))
+        new_cash = float(request.form.get("newCash"))
 
-        if not newCash:
+        # Ensure deposit amount was subitted
+        if not new_cash:
             return apology("Add deposit amount")
         
-        if newCash < 0:
+        # Ensure deposit greater than zero
+        if new_cash < 0:
             return apology("Deposit must be greater than zero")
 
+        # Query database to select cash balance of user
         with con:
             cur = con.cursor()
             cur.execute("SELECT cash FROM users WHERE id = ?", [session["user_id"]])
             cash = cur.fetchone()
 
-        updtCash = int(cash[0]) + newCash
+        # Get updated cash balance
+        updt_cash = float(cash[0]) + new_cash
+
+        # Query database to update cash balance in users table
         with con:
             cur = con.cursor()
-            cur.execute("UPDATE users SET cash = ? WHERE id = ?", (updtCash, session["user_id"]))
-        
-        flash(f"Deposited {usd(newCash)}!")
+            cur.execute("UPDATE users SET cash = ? WHERE id = ?", (updt_cash, session["user_id"]))
+
+        # Show flash message upon complete deposit   
+        flash(f"Deposited {usd(new_cash)}!")
         return redirect("/addCash")
 
 
@@ -226,15 +259,21 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    ticker = request.form.get("symbol")
+    symbol = request.form.get("symbol")
 
+    # User reached route via POST (as by submitting a form via POST)    
     if request.method == "POST":
-        symbol = lookup(ticker)
-        if symbol == None:
+
+        # Get quote for requested stock symbol
+        quote = lookup(symbol)
+
+        # Ensure symbol has been submitted
+        if quote == None:
             return apology("Symbol not found")
         else:
-            return render_template("quoted.html", symbol=symbol)
-
+            return render_template("quoted.html", symbol=quote)
+        
+    # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("quote.html")
 
@@ -243,28 +282,34 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
+
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
         username = request.form.get("username")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
 
+        # Query database for registered usernames
         with con:
             cur = con.cursor()
             cur.execute("SELECT username FROM users")
             usernames = cur.fetchall()
 
+        # Ensure username is not already taken
         for row in usernames:
-            if username == row:
+            if username == row[0]:
                 return apology("username already exists")
 
-
+        # Ensure username has been submitted
         if not username:
             return apology("username blank")
 
+        # Ensure password has been submitted, and that password is the same as confirmation password
         if not password or password != confirmation:
             return apology("password blank/does not match")
         else:
+            # Query database to insert username and generated password hash into users table
             with con:
                 con.execute("INSERT INTO users (username, hash) VALUES (?, ?)", (username, generate_password_hash(password)))
 
@@ -279,52 +324,73 @@ def register():
 def sell():
     """Sell shares of stock"""
 
+    # User reached route via GET (as by clicking a link or via redirect)
     if request.method == "GET":
+
+        # Query database for symbols of stocks owned by user so they can be displayed in dropdown list
         with con:
             cur = con.cursor()
             cur.execute("SELECT symbol FROM transactions WHERE buyer = ? GROUP BY name HAVING SUM(shares) > 0 ORDER BY symbol ASC", [session["user_id"]])
             symbols = cur.fetchall()
         
-        return render_template("sell.html", names = symbols)
-
+        return render_template("sell.html", symbols = symbols)
+    
+    # User reached route via POST (as by submitting a form via POST)
     else:
-        stockToSell = request.form.get("symbol")
-        sharesToSell = int(request.form.get("shares"))
+        stock_to_sell = request.form.get("symbol")
+        shares_to_sell = int(request.form.get("shares"))
 
-        if not stockToSell:
+        # Ensure stock was submitted
+        if not stock_to_sell:
             return apology("Must give symbol")
 
-        if sharesToSell < 0:
+        # Ensure valid number of shares was submitted
+        if shares_to_sell < 0:
             return apology("Shares must be greater than 0")
 
+        # Query database for number of shares that the user owns
         with con:
             cur = con.cursor()
-            cur.execute("SELECT SUM(shares) AS shares FROM transactions WHERE symbol = ?", [stockToSell])
+            cur.execute("SELECT SUM(shares) AS shares FROM transactions WHERE symbol = ?", [stock_to_sell])
             userShares = cur.fetchone()
 
         shares = int(userShares[0])
 
-        if shares < sharesToSell:
+        # Ensure the user has enough shares to sell
+        if shares < shares_to_sell:
             return apology("You don't have that many shares")
         else:
+
+            # Query database for user cash and convert to a float
             with con:
                 cur = con.cursor()
                 cur.execute("SELECT cash FROM users WHERE id = ?", [session["user_id"]])
-                userCash = cur.fetchone()[0]
+                user_cash = cur.fetchone()[0]
 
-            look = lookup(stockToSell)
-            price = look["price"]
+            cash = float(user_cash)
 
-            cash = int(userCash)
+            # Return a quote for the stock to sell
+            quote = lookup(stock_to_sell)
 
-            transaction_value = price * sharesToSell
-            updtCash = cash + transaction_value
+            # Get price of stock to sell as a float
+            price = quote["price"]
+
+            # Get transaction value as a float
+            transaction_value = price * float(shares_to_sell)
+
+            # Get new cash balance as a float
+            updt_cash = cash + transaction_value
+
+            # Get time of transaction
             time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+            # Query database to insert transaction data into transaction table
+            # Query database to update cash balance in users table
             with con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO transactions (buyer, name, symbol, transaction_price, shares, date, transaction_total, current_price) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (session["user_id"], look["name"], look["symbol"], price, (-1*sharesToSell), time, (-1*transaction_value), look["price"]))
-                cur.execute("UPDATE users SET cash = ? WHERE id = ?", (updtCash, session["user_id"]))
+                cur.execute("INSERT INTO transactions (buyer, name, symbol, transaction_price, shares, date, transaction_total, current_price) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (session["user_id"], quote["name"], quote["symbol"], price, (-1*shares_to_sell), time, (-1*transaction_value), quote["price"]))
+                cur.execute("UPDATE users SET cash = ? WHERE id = ?", (updt_cash, session["user_id"]))
 
+            # Show flash message upon complete transaction
             flash(f"Sold for {usd(transaction_value)}!")
             return redirect("/")
--- helpers.py
+++ helpers.py
@@ -7,26 +7,22 @@ from functools import wraps
 
 
 def apology(message, code=400):
-    """Render message as an apology to user."""
-    def escape(s):
-        """
-        Escape special characters.
+    # Render message as an apology to user
 
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+    # Special characters must be replaced as they are inserted into a URL on apology.html
+    def escape(s):
+        for new, old in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
                          ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
+            s = s.replace(new, old)
         return s
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
 def login_required(f):
-    """
-    Decorate routes to require login.
-
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
-    """
+    # Decorate routes to require login
+    # https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    # *args allows you to pass a varying number of positional arguments
+    # **kwargs allows the use of keyword arguments
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
@@ -36,20 +32,25 @@ def login_required(f):
 
 
 def lookup(symbol):
-    """Look up quote for symbol."""
+    # Look up quote for symbol
 
     # Contact API
     try:
+        # Retrieve API from evironment variable, set API_KEY in command line with: export API_KEY=value
         api_key = os.environ.get("API_KEY")
         url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
         response = requests.get(url)
+        # If error occurs, return a HTTPError object
         response.raise_for_status()
     except requests.RequestException:
         return None
 
     # Parse response
     try:
+        # Returns a json object
         quote = response.json()
+        
+        # Returns a dictionary with 3 keys
         return {
             "name": quote["companyName"],
             "price": float(quote["latestPrice"]),
@@ -58,7 +59,7 @@ def lookup(symbol):
     except (KeyError, TypeError, ValueError):
         return None
 
-
+# Formats argument into USD format e.g. 123.4528 = $123.45
 def usd(value):
     """Format value as USD."""
     return f"${value:,.2f}"
